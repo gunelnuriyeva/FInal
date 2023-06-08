86389e323a19fa69160f0fc87773996bacb90181
gunelnuriyeva <135689345+gunelnuriyeva@users.noreply.github.com>
2023-06-08 09:42:46 +0400

    Add files via upload

---
+++ cute-wheather-apps-react-master/README.md
@@ -0,0 +1,26 @@
+<h1>Simple Wheather ReactJs Aplication </h1>
+<img src="https://i.imgur.com/HRPHjFn.png">
+<br/>
+
+<a href="https://reactappsclima.herokuapp.com/"> DEMO </a>
+
+ <h2> How to use </h2>
+ <br/>
+ Get Api for Google Maps 
+ <b>index.html</b>
+ <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=[YOUR-API-KEY]&libraries=geometry,places"></script>
+ <br/>
+ 
+ Get Openweathermap api 
+ <b>app.js</b>
+ const api_call= await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade},Br&appid=${API_KEY}`);
+ 
+ 
+ <h2> References </h2>
+ 
+<li > <a href="https://reactjs.org/"> ReactJs </a></li>  
+<li > <a href="https://getbootstrap.com/docs/4.0/getting-started/introduction/"> Bootstrap 4 </a></li>  
+<li > <a href="https://nodejs.org/en/"> NodeJs </a></li>  
+<li > <a href="https://openweathermap.org/api"> Open Weather API </a></li>
+<li > <a href="https://www.etsy.com/shop/LookLookPrettyPaper?ref=l2-shopheader-name">Icones </a></li>  
+<li > <a href="https://github.com/ErrorPro/react-google-autocomplete/ " > React Google Maps </a> </li>
---
+++ cute-wheather-apps-react-master/src/App.js
@@ -0,0 +1,133 @@
+import React from "react";
+import Header from "./components/Header";
+import Form from "./components/Form";
+import Clima from "./components/Clima";
+
+const API_KEY = "[YOUR-KEY]";
+
+
+
+class App extends React.Component{
+
+  state = {
+    city : undefined,
+    temperature: undefined,
+    country: undefined,
+    humidity: undefined,
+    description: undefined,
+    erro : undefined
+
+  }
+
+
+  getClima = async (e) =>{    
+
+    e.preventDefault();    
+
+    const cidade = e.target.elements.city.value;
+ 
+    const api_call= await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade},Br&appid=${API_KEY}`);
+    const api_call2= await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${cidade},Br&appid=${API_KEY}`);
+    const data = await api_call.json();
+    const data2 = await api_call2.json();
+    const datas = [];
+    var j = 1;
+    var today = new Date();
+
+    console.log(today.getDate());
+    
+
+     
+    
+  
+
+
+    console.log(datas);
+    
+
+   
+    
+    if (cidade) {
+
+      this.setState({
+        city:data.name,
+        temperature: data.main.temp,
+        country:data.sys.country,
+        humidity:data.main.humidity,
+        description: data.weather[0].description,
+        descriptionIcon: data.weather[0].icon,
+        dia: today.getDate(),
+        mes: today.getMonth(),
+        erro:''
+        
+      });
+
+      //console.log(data);
+
+    
+
+    }else{
+
+
+      this.setState({
+        
+        erro :  '*'
+        
+      });
+
+
+    
+
+    }
+  
+    
+
+    
+    
+  }
+  render(){
+    return(
+      
+
+  <div className="container-fluid">
+<br/>
+
+  <Form getClima={this.getClima} getcountry={this.getcountry}/> 
+<br/>
+
+   <div className="col-md-4">
+      <div className="card text-white bg-primary mb-3" >
+        <div className="card-header"><h1><Clima city={this.state.city}/> </h1>{this.state.dia} / {this.state.mes}</div>
+        <div className="card-body">
+          <h5 className="card-title"><center><Clima temperature={this.state.temperature} /> </center></h5>
+          
+          <p className="card-text"><center><Clima description={this.state.description}/></center></p>
+        </div>
+
+       
+
+      
+      </div>
+    </div>
+
+
+
+   
+
+
+
+  </div>
+
+    
+
+      
+      
+
+       
+   
+
+    );
+  }
+};
+
+export default App;
